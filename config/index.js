bfa0fb58d022277c8e5eb62a9e20f84012eb3673
SevenOutman <leishenghao@126.com>
2018-04-05 01:49:33 +0800

    update build config

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'https://sevenoutman.github.io/Hubble/',
+    assetsPublicPath: '//sevenoutman.github.io/Hubble/',
 
     /**
      * Source Maps
