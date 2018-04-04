f71abb9b12fbd91b290c98a9623003e6bdf28eb2
Doma <leishenghao@126.com>
2018-04-04 19:53:46 +0800

    Add favicon

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -55,7 +55,8 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'index.html',
-      inject: true
+      inject: true,
+      favicon: path.resolve(__dirname, '../src/assets/logo.png')
     }),
     // copy custom static assets
     new CopyWebpackPlugin([
