167682b2a9208bfbc4f6e3b95baef8e00335bb80
SevenOutman <leishenghao@126.com>
2018-04-07 22:26:11 +0800

    fix when setting access token don't update request headers

--- src/graphql/apollo.js
+++ src/graphql/apollo.js
@@ -1,16 +1,27 @@
 import Vue from 'vue'
 import { ApolloClient } from 'apollo-client'
+import { ApolloLink } from 'apollo-link'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import VueApollo from 'vue-apollo'
 
-const httpLink = new HttpLink({
-  // You should use an absolute URL here
-  uri: 'https://api.github.com/graphql',
-  headers: {
-    Authorization: `token ${localStorage.getItem('access_token')}`
-  }
-})
+// @see https://github.com/apollographql/apollo-client/issues/2168#issuecomment-334122648
+const httpLink = ApolloLink.from([
+  (operation, forward) => {
+    operation.setContext(context => ({
+      ...context,
+      headers: {
+        ...context.headers,
+        Authorization: `token ${localStorage.getItem('access_token')}`
+      }
+    }))
+    return forward(operation)
+  },
+  new HttpLink({
+    // You should use an absolute URL here
+    uri: 'https://api.github.com/graphql'
+  })
+])
 
 // Create the apollo client
 const apolloClient = new ApolloClient({
