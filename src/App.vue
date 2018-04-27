4998fbd2a673641fa4fecaad47abed0b250a102e
SevenOutman <leishenghao@126.com>
2018-04-27 22:46:17 +0800

    add link to why access token is preferred

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@
         Follow <a
         href="https://github.com/settings/tokens/new?scopes=repo&description=Hubble"
         target="_blank"
-      >this link</a> to create one and paste it below (recommended).
+      >this link</a> to create one and paste it below (<a href="https://github.com/SevenOutman/Hubble/tree/netlify#why-is-access-token-preferred-rather-than-login-with-github" target="_blank">recommended</a>).
       </p>
       <div class="input-group" style="width: auto">
         <el-input v-model="accessToken" placeholder="Paste access token here">
@@ -38,7 +38,7 @@
         <svg class="octicon octicon-mark-github" viewBox="0 0 16 16" version="1.1" width="24" height="24" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>
         Login with GitHub</el-button>
       <a
-        href="https://github.com/SevenOutman/Hubble#access-token"
+        href="https://github.com/SevenOutman/Hubble/tree/netlify#why-is-it-required"
         target="_blank"
         style="margin: 1em 0 -1em;display: block"
       >Why is this required?</a>
