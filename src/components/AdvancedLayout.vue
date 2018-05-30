0aacd9ef5798b355b5fc53baa1cb955f33a4e205
SevenOutman <leishenghao@126.com>
2018-05-30 19:36:15 +0800

    add advanced layout

---
+++ src/components/AdvancedLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="layout advanced">
+    <header>
+
+    </header>
+    <aside>
+
+    </aside>
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "AdvancedLayout",
+  }
+</script>
+
+<style lang="less">
+  .layout.advanced {
+
+  }
+</style>
---
+++ src/components/MultiRepo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    MultiRepo
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "MultiRepo",
+  }
+</script>
+
+<style scoped>
+
+</style>
