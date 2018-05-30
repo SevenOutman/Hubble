a58717af354f6bd86d0c028d8e79bc8e88873866
SevenOutman <leishenghao@126.com>
2018-05-30 19:36:15 +0800

    add advanced layout

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import BasicLayout from '@/components/BasicLayout'
 import Home from '@/components/Home'
 import MyStars from '@/components/MyStars'
+import AdvancedLayout from '@/components/AdvancedLayout'
+import MultiRepo from '@/components/MultiRepo'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      component: Home
+      path: '/advanced',
+      component: AdvancedLayout,
+      children: [
+        {
+          path: '',
+          component: MultiRepo
+        }
+      ]
     },
     {
-      path: '/my-stars-this-year',
-      component: MyStars
-    }
+      path: '',
+      component: BasicLayout,
+      children: [
+        {
+          path: '/',
+          component: Home
+        },
+        {
+          path: '/my-stars-this-year',
+          component: MyStars
+        }
+      ]
+    },
   ]
 })
