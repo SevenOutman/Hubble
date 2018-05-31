0d6c753b7fa82e938c8a9215202970cc74c3e2dc
SevenOutman <leishenghao@126.com>
2018-05-31 20:03:58 +0800

    feat: repo race

--- src/router/index.js
+++ src/router/index.js
@@ -3,23 +3,12 @@ import Router from 'vue-router'
 import BasicLayout from '@/components/BasicLayout'
 import Home from '@/components/Home'
 import MyStars from '@/components/MyStars'
-import AdvancedLayout from '@/components/AdvancedLayout'
 import MultiRepo from '@/components/MultiRepo'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/advanced',
-      component: AdvancedLayout,
-      children: [
-        {
-          path: '',
-          component: MultiRepo
-        }
-      ]
-    },
     {
       path: '',
       component: BasicLayout,
@@ -31,6 +20,10 @@ export default new Router({
         {
           path: '/my-stars-this-year',
           component: MyStars
+        },
+        {
+          path: '/repo-race',
+          component: MultiRepo
         }
       ]
     },
